c208e23f42a9c1a97054f77758460b3db2ae1119
ekwe7 <ekwea753@gmail.com>
2026-05-07 14:59:52 +0100

    initial commit: add zeeplus-client and zeePlus-server

---
+++ zeePlus-server/src/main/java/com/ekwe_hub/zeeplusserver/ZeePlusServerApplication.java
@@ -0,0 +1,13 @@
+package com.ekwe_hub.zeeplusserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeePlusServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeePlusServerApplication.class, args);
+    }
+
+}
---
+++ zeePlus-server/src/test/java/com/ekwe_hub/zeeplusserver/ZeePlusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekwe_hub.zeeplusserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeePlusServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
